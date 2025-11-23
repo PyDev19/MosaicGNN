a34fad246fcd9fb5e6ec8ee6863ad9a403e1718b
Atharva Mishra <atharvamishra956+1@gmail.com>
2025-11-23 13:35:54 -0600

    main file to handle all training by initializing different modules

--- src/main.py
+++ src/main.py
@@ -1,9 +1,28 @@
+import torch
 import yaml
+from dataset import RecommenderDataModule
+from model import RecommenderModelModule
 from trainer import Trainer
 
-with open('config.yaml', 'r') as file:
-    config = yaml.safe_load(file)
+DATA_DIR = "data/"
 
-gnn_trainer = Trainer(config)
+config = yaml.safe_load(open("config.yaml", "r"))
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+data_module = RecommenderDataModule(DATA_DIR, config["dataset"], config["loader"])
+model_module = RecommenderModelModule(
+    config["model"], config["optimizer"], config["scheduler"], device
+)
+
+data_module.setup()
+
+gnn_trainer = Trainer(
+    **config["trainer"],
+    **config["early_stopping"],
+    device=device,
+    data_module=data_module,
+    model_module=model_module,
+)
 
 gnn_trainer.train()
+gnn_trainer.save("results")
