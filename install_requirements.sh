70c21e7e9cae10ea885dbd633d1c0e275f0f30b0
Atharva Mishra <atharvamishra956+1@gmail.com>
2025-11-23 13:48:02 -0600

    install script for kaggle

---
+++ install_requirements.sh
@@ -0,0 +1,4 @@
+pip install cmake ninja wheel
+pip install torch_geometric
+pip install --no-build-isolation git+https://github.com/pyg-team/pyg-lib.git
+pip install torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.6.0+cu124.html
