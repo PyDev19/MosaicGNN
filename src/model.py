e7ceb369354fe47f6b2c16deb755e8ebb2622c1a
Atharva Mishra <atharvamishra956+1@gmail.com>
2025-11-23 13:35:20 -0600

    improved message passing in encoder layer by adding residual connection and created module class to store the model, optimizer, and scheduler

--- src/model.py
+++ src/model.py
@@ -1,10 +1,19 @@
 import torch
-from torch.nn import Module, Linear, Embedding, Dropout, Sequential, ReLU
+from torch.nn import (
+    Module,
+    Linear,
+    Embedding,
+    Dropout,
+    Sequential,
+    ReLU,
+)
+from torch.optim import AdamW
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch_geometric.nn import SAGEConv, HeteroConv
 
 
 class NodeEmbeddings(Module):
-    def __init__(self, num_users, num_movies, embed_dim, movie_feat_dim, dropout=0.3):
+    def __init__(self, num_users, embed_dim, movie_feat_dim, dropout=0.3):
         super().__init__()
         self.user_emb = Embedding(num_users, embed_dim)
         self.proj_movie = Sequential(
@@ -27,7 +36,7 @@ class SAGE_Encoder(Module):
             {
                 ("user", "rates", "movie"): SAGEConv((-1, -1), hidden_channels),
                 ("movie", "rev_rates", "user"): SAGEConv((-1, -1), hidden_channels),
-            }
+            },
         )
         self.conv2 = HeteroConv(
             {
@@ -38,17 +47,17 @@ class SAGE_Encoder(Module):
 
         self.dropout = Dropout(dropout)
 
-    def forward(self, x_dict, edge_index_dict, edge_weight_dict):
+    def forward(self, x_dict, edge_index_dict):
         h0 = x_dict
-        
-        x_dict = self.conv1(x_dict, edge_index_dict, edge_weight_dict)
-        x_dict = {k: self.dropout(x.relu()) for k, x in x_dict.items()}
-        
-        res = {k: x0 + x1 for (k, x0), (_, x1) in zip(h0.items(), x_dict.items())}
 
-        x_dict = self.conv2(res, edge_index_dict, edge_weight_dict)
+        x_dict = self.conv1(x_dict, edge_index_dict)
+        x_dict = {k: self.dropout(x.relu()) for k, x in x_dict.items()}
+
+        res = {k: h0[k] + x_dict[k] for k in x_dict}
+
+        x_dict = self.conv2(res, edge_index_dict)
         x_dict = {k: self.dropout(x) for k, x in x_dict.items()}
-        
+
         return x_dict
 
 
@@ -73,27 +82,19 @@ class NOVA_GNN(Module):
     def __init__(
         self,
         num_users=100,
-        num_movies=100,
         movie_feat_dim=128,
         hidden_channels=64,
         dropout=0.3,
     ):
         super().__init__()
-        self.embed = NodeEmbeddings(
-            num_users, num_movies, hidden_channels, movie_feat_dim, dropout
-        )
+        self.embed = NodeEmbeddings(num_users, hidden_channels, movie_feat_dim, dropout)
         self.gnn = SAGE_Encoder(hidden_channels, dropout)
         self.decoder = EdgeDecoder(hidden_channels)
 
     def forward(self, data):
         x_dict = self.embed(data)
 
-        edge_weights = {
-            key: torch.ones_like(attr, dtype=torch.float32)
-            for key, attr in data.edge_attr_dict.items()
-        }
-
-        x_dict = self.gnn(x_dict, data.edge_index_dict, edge_weights)
+        x_dict = self.gnn(x_dict, data.edge_index_dict)
         edge_label_index = data["user", "rates", "movie"].edge_label_index
 
         for k in x_dict:
@@ -101,3 +102,19 @@ class NOVA_GNN(Module):
 
         out = self.decoder(x_dict["user"], x_dict["movie"], edge_label_index)
         return out
+
+
+class RecommenderModelModule:
+    def __init__(
+        self,
+        model_config: dict,
+        optimizer_config: dict,
+        scheduler_config: dict,
+        device: torch.device,
+    ):
+        self.model = NOVA_GNN(**model_config).to(device)
+        self.optimizer = AdamW(self.model.parameters(), **optimizer_config)
+        self.scheduler = ReduceLROnPlateau(self.optimizer, **scheduler_config)
+
+    def parameters(self):
+        return self.model.parameters()
