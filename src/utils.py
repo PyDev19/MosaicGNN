5d399deb20b9713351a1556bf90586f2628c254c
Atharva Mishra <atharvamishra956+1@gmail.com>
2025-11-23 13:34:20 -0600

    created a utils files to store usefull utilities such as loggin and metrics

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+from sklearn.metrics import average_precision_score, roc_auc_score
+from torch.utils.tensorboard import SummaryWriter
+
+
+class Metrics:
+    @staticmethod
+    def compute(preds, labels):
+        return {
+            "auc": roc_auc_score(labels, preds),
+            "ap": average_precision_score(labels, preds),
+        }
+
+
+class TBLogger:
+    def __init__(self, logdir: str):
+        self.writer = SummaryWriter(logdir)
+
+    def log(self, key, value, step):
+        self.writer.add_scalar(key, value, step)
+
+    def close(self):
+        self.writer.flush()
+        self.writer.close()
